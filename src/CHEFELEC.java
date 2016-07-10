324432801a0df3e1db8ba4fe6af5c9c912be2d8b
Andrii Mudrevskyi <amudrevskyi@kbp-amud.spscommerce.com>
2016-07-10 19:45:06 +0300

    july challenge

--- src/CHEFELEC.java
+++ src/CHEFELEC.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//test
 public class CHEFELEC {
 
     private static int[] converter(String[] input) {
