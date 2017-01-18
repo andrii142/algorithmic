1660796ea8eb54c827ba5961356b4f2afc22c5c0
Andrii <amudrevskiy@gmail.com>
2017-01-18 23:42:25 +0200

    kids stuff

---
+++ src/eolymp/c++/main_test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int a = 100;
+    std::cout << a << std::endl;
+
+    std::cout << "Hello, World!" << std::endl;
+        int a = 100;
+        std::cout << a << std::endl;
+
+        std::cout << "Hello, World!" << std::endl;
+            int a = 100;
+            std::cout << a << std::endl;
+
+            std::cout << "Hello, World!" << std::endl;
+                int a = 100;
+                std::cout << a << std::endl;
+
+
+                std::cout << "Hello, World!" << std::endl;
+                    int a = 100;
+                    std::cout << a << std::endl;
+
+
+                    std::cout << "Hello, World!" << std::endl;
+                        int a = 100;
+                        std::cout << a << std::endl;
+
+
+                        std::cout << "Hello, World!" << std::endl;
+                            int a = 100;
+                            std::cout << a << std::endl;
+    return 0;
+}
